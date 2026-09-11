37f1da9942d429d1844a0480f07dde70bdb31c26
Martin Lamacchia <martinjiro22@gmail.com>
2026-09-11 10:29:22 -0300

    Terminado el Slice del login

--- src/store/features/users/loginSlice.js
+++ src/store/features/users/loginSlice.js
@@ -1,51 +1,77 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const loginUser = createAsyncThunk(
+    "login/loginUser",
+    async (cedential, { rejectWithValue }) => {
+        try {
+            const { data } = await axios.post(
+              // `${import.meta.env.VITE_API_URL}/user/login`,
+              "http://localhost:3001/user/register",
+              cedential
+            )
+            return data
+        } catch (error) {
+            const code = error.response?.data?.code || "network_error"
+            return rejectWithValue(code)
+        }
+    }
+)
+
+// Funcion Leer LocalStorage
+const loadUserFromStorage = () => {
+  try {
+    const storeUser = localStorage.getItem("user")
+    return storeUser ? JSON.parse(storeUser) : null
+  } catch (error) {
+    return null
+  }
+}
 
 const initialState = {
-  user: null,
-  isAuthenticated: false,
-};
+  user: loadUserFromStorage(),
+  isAuthenticated: !!loadUserFromStorage(),
+  loading: false,
+  error: null,
+  successCode: null
+}
 
 const loginSlice = createSlice({
   name: "login",
   initialState,
   reducers: {
-    loginSuccess: (state, action) => {
-      state.user = action.payload;
-      state.isAuthenticated = true;
-    },
     logout: (state) => {
-      state.user = null;
-      state.isAuthenticated = false;
+      state.user = null
+      state.isAuthenticated = false
+      state.error = null
+      state.successCode = null
+      localStorage.removeItem('user')
     },
+    resetLoginState: (state) => {
+      state.error = null
+      state.successCode = null
+    } 
   },
-});
+  extraReducers: (builder) {
+    builder
+    .addCase(loginUser.pending, (state) => {
+      state.loading = true
+      state.error = null
+    })
+    .addCase(loginUser.fulfilled, (state, action) => {
+      state.loading = false
+      state.isAuthenticated = true
+      state.successCode = action.payload.code
+      state.user = action.payload.findUser
+      localStorage.setItem("user", JSON.stringify(action.payload.findUser))
+    })
+    .addCase(loginUser.rejected, (state, action) => {
+      state.loading = false
+      state.isAuthenticated = false
+      state.error = action.payload
+    })
+  }
+})
 
-
-export const { loginSuccess, logout } = loginSlice.actions;
-export default loginSlice.reducer;
-
-export const fetchUserLogin = (form) => {
-  return async (dispatch) => {
-    try {
-      const { data } = await axios.post(
-        "http://localhost:3001/user/login",
-        form
-      );
-      dispatch(loginSuccess({ user: data.findUser, isAuthenticated: data.access }));
-      return data.access;
-    } catch (error) {
-      dispatch(loginFailure(error.response.data.message));
-    }
-  };
-};
-
-export const fetchUserLogout = () => {
-  return async (dispatch) => {
-    try {
-      dispatch(loginSuccess({ user: null, access: false, message: "" }));
-      return false;
-    } catch (error) {
-      console.log({Error: error});
-    }
-  };
-};
+export const { logout, resetLoginState } = loginSlice.actions
+export default loginSlice.reducer
